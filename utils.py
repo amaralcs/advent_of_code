2c1852075d8dc6010bc85523b0b11fe2221b36da
amaralcs <amaralcscgp@gmail.com>
2020-12-07 18:16:24 +0000

    added multiline parser

--- utils.py
+++ utils.py
@@ -3,3 +3,8 @@ def read_file(fpath):
     with open(fpath) as f:
         data = f.readlines()
         return [val.strip() for val in data]
+
+def parse_multiline(data, sep="?"):
+    separated = [val if val!= "" else sep for val in data]
+    concat = " ".join(separated)
+    return concat.split(sep)
