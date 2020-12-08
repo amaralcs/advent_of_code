9c7e83c529c4bb649effdcfc09bdb26c14695590
amaralcs <amaralcscgp@gmail.com>
2020-12-08 21:59:02 +0000

    solved day 8

--- day8/solution.py
+++ day8/solution.py
@@ -10,18 +10,30 @@ def run_op(op, val, acc, idx):
         return acc, idx + 1
 
 def run_copy(data, idx, op, val, verbose):
-    if verbose: print(f"  Running copy by swapping {op} in idx {idx}")
+    """Copies data, modifies the operation (if jmp or nop) then runs the program with the modified data. 
+    If we're able to reach the end of the program we found the location where the change needs to happen.
+    """
     reverse_op = "jmp" if op == "nop" else "nop"
     data_cp = data.copy()
     data_cp[idx] = f"{reverse_op} {str(val)}"
-    if run_code(data_cp, modded=True, verbose=verbose) == True:
-        print(f"  Found that changing {op} {val} at idx {idx} works!")
-        return f"{reverse_op} {str(val)}", True
+
+    if verbose: print(f"  Running copy by swapping {op} in idx {idx}")
+    finished_run, acc = run_code(data_cp, modded=True, verbose=verbose)
+    if finished_run:
+        if verbose: print(f"  Found that changing {op} {val} at idx {idx} works! acc: {acc}")
+        return reverse_op, val, True  # Mod the operation and tell main function we modded it
     else:
-        return f"{op} {str(val)}", False
+        return op, val, False
 
 
-def run_code(data, verbose=False, modded=False):
+def run_code(data, modded=False, verbose=False):
+    """Loops through a program executing its operations:
+
+    :param data: List of instructions to execute
+    :param verbose: Level of verbosity, useful for debugging
+    :param modded: Whether we're running a modified copy of program. Set this to True
+    to find answer to the first part
+    """
     visited = [0 for _ in range(len(data))]
     acc, idx = 0, 0
     mod_space = " "*4 if modded else ""
@@ -29,31 +41,28 @@ def run_code(data, verbose=False, modded=False):
         try:
             op, val = data[idx].split(" ")
             val = int(val)
-        except IndexError:
-            if verbose: print(f"{mod_space}Found index error")
-            if modded: return True
-            else: return acc
+        except IndexError:  # IndexError means we tried to go out of the list
+            if verbose: print(f"{mod_space}Found index error idx: {idx}, op {op}, val {val}")
+            return modded, acc
         if verbose: print(f"{mod_space}idx {idx}: {op} {val}; visited {visited[idx]};")
 
-        if not modded and op != "acc":
-            data[idx], modded = run_copy(data, idx, op, val, verbose=verbose)
+        if not modded and op != "acc":  # Only run a copy if we haven't modified the program
+            op, val, modded = run_copy(data, idx, op, val, verbose=verbose)
             if verbose: print(f"Back to main function")
 
         if visited[idx] == 1:
-            return acc
-
+            return False, acc
         visited[idx] = 1
         acc, idx = run_op(op, val, acc, idx)
-    
+
 
 if __name__ == "__main__":
     data = read_file("day8/data.txt")
-    acc = run_code(data, modded=True)
+    _, acc = run_code(data, modded=True)
     print(f"Value of acc before program loops: {acc}")
     
-    dummy = read_file("day8/dummy.txt")
+    dummy = read_file("day8/test.txt")
     run_code(dummy, verbose=True)
 
-    run_code(data, True, True)
-    run_code(data, True)
-    run_code(data, False)
+    _, acc = run_code(data, False)
+    print(f"Value of acc after running the full program: {acc}")
